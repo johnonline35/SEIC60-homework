9960a73e5b09f40113f7ab5ecc09a758bb6f2a90
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-01-30 16:24:25 +1100

    Finished

--- 07-John-Childs-Eddy/wk07/1-mon/js/googlebooks.js
+++ 07-John-Childs-Eddy/wk07/1-mon/js/googlebooks.js
@@ -1,6 +1,7 @@
-const titleSearch = function (bookTitle) {
+const titleSearch = function () {
+    const title = document.getElementById('query').value
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=title:' + bookTitle );
+    xhr.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=title:' + title );
     xhr.send(); // This line is asynchronous
 
     xhr.onreadystatechange = function () {
@@ -9,14 +10,10 @@ const titleSearch = function (bookTitle) {
         }
 
         const data = JSON.parse( xhr.responseText ); // Turn the string into a JSON object using .parse
-
+        // console.log(data)
         
         const p = document.createElement('IMG');
-        p.src = data.item.volumeInfo.imageLinks.thumbnail; // .text is the key for the data object which returns the value for the text key.
-        document.body.prepend(p);
-        
+        p.src = data.items[0].volumeInfo.imageLinks.thumbnail; // .text is the key for the data object which returns the value for the text key.
+        document.body.appendChild(p);
     };
 };
-
-const form = document.getElementById('search');
-form.addEventListener('submit', titleSearch(form));

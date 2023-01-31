98171d2daf5dd4f74f30f53bc63034d92b357068
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-01-31 15:09:03 +1100

    Finished homework

---
+++ 07-John-Childs-Eddy/wk07/2-tue/js/api.js
@@ -0,0 +1,25 @@
+$('#form').on('submit', function(event) {
+    event.preventDefault();
+    $.ajax('https://api.coingecko.com/api/v3/search/trending').done(function(data) {
+    
+    for ( let i = 0; i < data.coins.length; i++ ) {
+        $('#form').append(`<h1>Coin Name: ${ data.coins[i].item.name }</h1>`)
+        $('#form').append(`<h3>Market Cap Rank: ${ data.coins[i].item.market_cap_rank }</h3>`)
+        $('#form').append(`<h3>Price in BTC: ${ data.coins[i].item.price_btc }</h3>`)
+        const $image = $('<img>').attr('src', data.coins[i].item.large);
+        $('#form').append($image)
+    }
+    // $('#form').after(`<h1>${ resultName }</h1>`)
+    const $image = $('<img>>').attr('src', data.coins[i].item.large);
+    $('body').append('$image')
+    });
+});
+
+// $('search').on('submit', function(event) {
+//     event.preventDefault();
+//     const title = $('title').val();
+//     $.ajax('https://api.coingecko.com/api/v3/search/trending' + title).done(function(data) {
+//         const result = data.items[0].volumeInfo.imageLinks.thumbnail;
+//         $('result').attr('src', result);
+//     });
+// });

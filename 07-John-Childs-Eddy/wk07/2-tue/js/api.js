7e4a279bc4f3de8b6497df0f196e1b87b48092a8
John Childs-Eddy <johnchildseddy@Johns-MacBook-Air.local>
2023-01-31 15:12:47 +1100

    Just formatted to make cleaner

--- 07-John-Childs-Eddy/wk07/2-tue/js/api.js
+++ 07-John-Childs-Eddy/wk07/2-tue/js/api.js
@@ -2,16 +2,13 @@ $('#form').on('submit', function(event) {
     event.preventDefault();
     $.ajax('https://api.coingecko.com/api/v3/search/trending').done(function(data) {
     
-    for ( let i = 0; i < data.coins.length; i++ ) {
-        $('#form').append(`<h1>Coin Name: ${ data.coins[i].item.name }</h1>`)
-        $('#form').append(`<h3>Market Cap Rank: ${ data.coins[i].item.market_cap_rank }</h3>`)
-        $('#form').append(`<h3>Price in BTC: ${ data.coins[i].item.price_btc }</h3>`)
-        const $image = $('<img>').attr('src', data.coins[i].item.large);
-        $('#form').append($image)
-    }
-    // $('#form').after(`<h1>${ resultName }</h1>`)
-    const $image = $('<img>>').attr('src', data.coins[i].item.large);
-    $('body').append('$image')
+        for ( let i = 0; i < data.coins.length; i++ ) {
+            $('#form').append(`<h1>Coin Name: ${ data.coins[i].item.name }</h1>`)
+            $('#form').append(`<h3>Market Cap Rank: ${ data.coins[i].item.market_cap_rank }</h3>`)
+            $('#form').append(`<h3>Price in BTC: ${ data.coins[i].item.price_btc }</h3>`)
+            const $image = $('<img>').attr('src', data.coins[i].item.large);
+            $('#form').append($image)
+        };
     });
 });
 
